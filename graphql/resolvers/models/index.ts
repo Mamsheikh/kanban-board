9dfcb6811f724aba82161b6c147e73a579848afe
Your Name <mamsheikh123@gmail.com>
2022-03-20 23:06:16 +0100

    ✨  GraphQL server setup

---
+++ graphql/resolvers/models/index.ts
@@ -0,0 +1,11 @@
+import { objectType } from 'nexus'
+
+export const User = objectType({
+  name: 'User',
+  definition(t) {
+    t.nonNull.string('id'), t.nonNull.string('name')
+    t.nonNull.string('email')
+    t.string('image')
+    t.nonNull.boolean('isAdmin')
+  },
+})
--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -2,10 +2,11 @@ import { ApolloServer } from 'apollo-server-micro'
 
 import Cors from 'micro-cors'
 import { createContext } from '../../graphql/context'
+import { schema } from '../../graphql/schema'
 
 const cors = Cors()
 
-const apolloServer = new ApolloServer({ context: createContext })
+const apolloServer = new ApolloServer({ schema, context: createContext })
 
 const startServer = apolloServer.start()
 
